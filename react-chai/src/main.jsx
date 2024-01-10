92ec40c85a317c5e1cedae391ca13f4647ae1e1d
Amirsohail335 <amirsohail335@gmail.com>
2024-01-10 22:20:39 +0530

    committed

--- react-chai/src/main.jsx
+++ react-chai/src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import ImageHeadingTitleComponent from "./components/ImageHeadingTitle.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <ImageHeadingTitleComponent/>
   </React.StrictMode>
 );
