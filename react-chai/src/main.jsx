f6c72f9671c3d190353ec207925969629387913e
Amirsohail335 <amirsohail335@gmail.com>
2024-02-20 16:28:11 +0530

    committed

--- react-chai/src/main.jsx
+++ react-chai/src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
     {/* <ImageHeadingTitleComponent />/ */}
     {/* <TableComponent/> */}
-    {/* <InputModal/> */}
-    <InputBoxExample/>
+    <InputModal/>
+    {/* <InputBoxExample/> */}
   </React.StrictMode>
 );
